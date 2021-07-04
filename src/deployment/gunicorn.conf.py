d5892d6689da83cb7d533ee0f197c255e11363e3
Jesús Fragoso <rust.in.peace.2010@gmail.com>
2021-07-04 18:24:19 -0500

    [FEATURE] - Adding the new gunicorn configuration file

--- src/deployment/gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-import os
-
-bind = os.environ.get("BIND_SERVER")
-workers = os.environ.get("WORKERS")
-timeout = os.environ.get("SERVER_TIMEOUT")
-keepalive = os.environ.get("KEEP_ALIVE_SERVER")
-reload = os.environ.get("RELOAD_SERVER")
